1f77a2d291bb367c8d2945bd84a618f95942df5a
FRANKD-UX <frankdafrica@gmail.com>
2025-01-24 03:32:03 -0800

    taks0

---
+++ helpers.c
@@ -0,0 +1,36 @@
+#include "monty.h"
+
+void free_stack(stack_t *stack)
+{
+    stack_t *temp;
+
+    while (stack)
+    {
+        temp = stack;
+        stack = stack->next;
+        free(temp);
+    }
+}
+
+void execute(char *opcode, stack_t **stack, unsigned int line_number)
+{
+    instruction_t instructions[] = {
+        {"pall", pall},
+        {NULL, NULL}
+    };
+    int i = 0;
+
+    while (instructions[i].opcode)
+    {
+        if (strcmp(opcode, instructions[i].opcode) == 0)
+        {
+            instructions[i].f(stack, line_number);
+            return;
+        }
+        i++;
+    }
+
+    fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+    exit(EXIT_FAILURE);
+}
+
---
+++ push_pall.c
@@ -0,0 +1,37 @@
+#include "monty.h"
+
+void push(stack_t **stack, unsigned int line_number, char *arg)
+{
+    stack_t *new_node;
+    int num;
+
+    if (!arg || sscanf(arg, "%d", &num) != 1)
+    {
+        fprintf(stderr, "L%d: usage: push integer\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    new_node = malloc(sizeof(stack_t));
+    if (!new_node)
+        malloc_error();
+
+    new_node->n = num;
+    new_node->prev = NULL;
+    new_node->next = *stack;
+    if (*stack)
+        (*stack)->prev = new_node;
+    *stack = new_node;
+}
+
+void pall(stack_t **stack, unsigned int line_number)
+{
+    stack_t *temp = *stack;
+
+    (void)line_number;
+    while (temp)
+    {
+        printf("%d\n", temp->n);
+        temp = temp->next;
+    }
+}
+
